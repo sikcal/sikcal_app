5ba01b8d5b7c3c8ec941d80f368b1a37cb93a6e6
hi-jin <crushed7@o.cnu.ac.kr>
2022-07-11 15:49:37 +0900

    add searchFood method (connect to server)

--- lib/screens/home/search_menu_view.dart
+++ lib/screens/home/search_menu_view.dart
@@ -1,69 +1,46 @@
 import 'package:bottom_bar/bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sikcal/data/provider.dart';
 import 'package:sikcal/model/food.dart';
 import 'package:sikcal/model/meal.dart';
 import 'package:sikcal/screens/components/food_list_view.dart';
 
 import '../../data/constants.dart';
 
-class SearchMenuView extends StatefulWidget {
+class SearchMenuView extends ConsumerStatefulWidget {
   SearchMenuView({Key? key, this.meal}) : super(key: key);
 
   Meal? meal;
 
   @override
-  State<SearchMenuView> createState() => _SearchMenuViewState();
+  ConsumerState<SearchMenuView> createState() => _SearchMenuViewState();
 }
 
-class _SearchMenuViewState extends State<SearchMenuView> {
+class _SearchMenuViewState extends ConsumerState<SearchMenuView> {
   int _currentPage = 2;
-
   late Meal meal;
+
+  List<Food> currentFoodList = [];
+  late TextEditingController controller;
 
   @override
   void initState() {
     meal = widget.meal ?? Meal();
+    controller = TextEditingController();
   }
 
-  // TODO : 서버에서 받아오기
-  List<Food> currentFoodList = [
-    Food(
-      hashId: 1,
-      name: '햇반(CJ) 1팩 (200g)',
-      carbohydrate: 67,
-      protein: 5,
-      fat: 1,
-      totalKcal: 300,
-    ),
-    Food(
-      hashId: 2,
-      name: '햇반(햇반) 1팩 (210g)',
-      carbohydrate: 70,
-      protein: 5,
-      fat: 1,
-      totalKcal: 315,
-    ),
-    Food(
-      hashId: 3,
-      name: '햇반(오뚜기) 1인분 (200g)',
-      carbohydrate: 68,
-      protein: 5,
-      fat: 1,
-      totalKcal: 310,
-    ),
-    Food(
-      hashId: 4,
-      name: '닭 가슴살 (100g)당',
-      carbohydrate: 1,
-      protein: 31,
-      fat: 4,
-      totalKcal: 250,
-    ),
-  ];
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final foodRepo = ref.watch(foodRepoProvider);
+
     return Scaffold(
       backgroundColor: const Color(0xffeeeeee),
       appBar: AppBar(
