dfa162a0fba7bd56b7cbd4aabfabedde541c6e14
Yujin_nKim <yujin.kimd@gmail.com>
2022-07-16 03:02:40 +0900

    fix mydiet_page about abb bar and bottom bar

--- lib/screen/mydiet/check_upload_mydiet.dart
+++ lib/screen/mydiet/check_upload_mydiet.dart
@@ -5,6 +5,10 @@ import 'package:sikcal/components/RoundedButton_size.dart';
 import '../../components/button_add_mydiet.dart';
 import '../../data/constants.dart';
 import '../../model/diet.dart';
+import '../../screens/feed/feed_view.dart';
+import '../../screens/home/home_view.dart';
+import '../mypage/mypage_main_view.dart';
+import 'mydiet_main_view.dart';
 
 
 class CheckUploadMyDiet extends ConsumerStatefulWidget {
@@ -21,10 +25,51 @@ class _CheckUploadMyDiet extends ConsumerState<CheckUploadMyDiet> {
 
   String? MealTime;
 
+  int _currentPage = 3; // 현재 페이지 (bottom nav bar 관련)
+
+  List<Widget> pages = [
+    FeedView(),
+    Container(),
+    const HomeView(),
+    const MyDietMainView(),
+    const MyPageMainView(),
+  ];
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: const [
+              Image(
+                image: AssetImage('images/fork.png'),
+                height: 25.0,
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Text(
+                "식칼", // FIXME 화면 별로 title 변경
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Image(
+                image: AssetImage('images/knife.png'),
+                height: 25.0,
+              ),
+            ],
+            mainAxisSize: MainAxisSize.min,
+          ),
+          backgroundColor: primaryColor,
+        ),
       body : Container(
           padding: EdgeInsets.all(8.0),
           child: Center(
@@ -90,7 +135,56 @@ class _CheckUploadMyDiet extends ConsumerState<CheckUploadMyDiet> {
                   )
                 ],
               ))
-      )
+      ),
+      bottomNavigationBar: BottomBar(
+        itemPadding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 20.0),
+        backgroundColor: primaryColor,
+        items: [
+          BottomBarItem(
+              icon: const Icon(
+                Icons.feed,
+                color: Colors.white,
+              ),
+              title: const Text("피드"),
+              activeColor: Colors.white),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.chat_bubble_outline,
+                color: Colors.white,
+              ),
+              title: const Text("그룹 채팅"),
+              activeColor: Colors.white),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.home_outlined,
+                size: 30.0,
+                color: Colors.white,
+              ),
+              title: const Text("홈 화면"),
+              activeColor: Colors.white),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.star_outline,
+                size: 30.0,
+                color: Colors.white,
+              ),
+              title: const Text("나의 식단"),
+              activeColor: Colors.white),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              title: const Text("마이페이지"),
+              activeColor: Colors.white),
+        ],
+        onTap: (int value) {
+          setState(() {
+            _currentPage = value;
+          });
+        },
+        selectedIndex: _currentPage,
+      ),
     );
   }
 
--- lib/screen/mydiet/mydiet_detail_view.dart
+++ lib/screen/mydiet/mydiet_detail_view.dart
@@ -1,9 +1,15 @@
+import 'package:bottom_bar/bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../components/button_add_mydiet.dart';
+import '../../data/constants.dart';
 import '../../model/diet.dart';
+import '../../screens/feed/feed_view.dart';
+import '../../screens/home/home_view.dart';
+import '../mypage/mypage_main_view.dart';
 import 'check_upload_mydiet.dart';
+import 'mydiet_main_view.dart';
 
 
 class MyDietDetailView extends ConsumerStatefulWidget {
@@ -16,6 +22,16 @@ class MyDietDetailView extends ConsumerStatefulWidget {
 
 class _MyDietDetailView extends ConsumerState<MyDietDetailView> {
 
+  int _currentPage = 3; // 현재 페이지 (bottom nav bar 관련)
+
+  List<Widget> pages = [
+    FeedView(),
+    Container(),
+    const HomeView(),
+    const MyDietMainView(),
+    const MyPageMainView(),
+  ];
+
   @override
   Widget build(BuildContext context) {
 
@@ -25,6 +41,37 @@ class _MyDietDetailView extends ConsumerState<MyDietDetailView> {
     String diet_fat = widget.diet.diet_fat.toString();
 
     return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: const [
+              Image(
+                image: AssetImage('images/fork.png'),
+                height: 25.0,
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Text(
+                "식칼", // FIXME 화면 별로 title 변경
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Image(
+                image: AssetImage('images/knife.png'),
+                height: 25.0,
+              ),
+            ],
+            mainAxisSize: MainAxisSize.min,
+          ),
+          backgroundColor: primaryColor,
+        ),
       body: Container(
           padding: EdgeInsets.all(8.0),
           child: Center(
@@ -59,7 +106,56 @@ class _MyDietDetailView extends ConsumerState<MyDietDetailView> {
                   //TODO : 조리 순서 출력
                 ],
               ))
-      )
+      ),
+      bottomNavigationBar: BottomBar(
+        itemPadding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 20.0),
+        backgroundColor: primaryColor,
+        items: [
+          BottomBarItem(
+              icon: const Icon(
+                Icons.feed,
+                color: Colors.white,
+              ),
+              title: const Text("피드"),
+              activeColor: Colors.white),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.chat_bubble_outline,
+                color: Colors.white,
+              ),
+              title: const Text("그룹 채팅"),
+              activeColor: Colors.white),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.home_outlined,
+                size: 30.0,
+                color: Colors.white,
+              ),
+              title: const Text("홈 화면"),
+              activeColor: Colors.white),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.star_outline,
+                size: 30.0,
+                color: Colors.white,
+              ),
+              title: const Text("나의 식단"),
+              activeColor: Colors.white),
+          BottomBarItem(
+              icon: const Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              title: const Text("마이페이지"),
+              activeColor: Colors.white),
+        ],
+        onTap: (int value) {
+          setState(() {
+            _currentPage = value;
+          });
+        },
+        selectedIndex: _currentPage,
+      ),
     );
   }
 }
