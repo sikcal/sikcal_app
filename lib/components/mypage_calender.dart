f33bef6f3e0706b640393820756add9a8f5ab707
Yujin_nKim <yujin.kimd@gmail.com>
2022-07-27 13:33:45 +0900

    m

--- lib/components/mypage_calender.dart
+++ lib/components/mypage_calender.dart
@@ -1,22 +1,47 @@
+import 'dart:convert';
+import 'dart:core';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:http/http.dart' as http;
 
+import '../data/shared_preferences.dart';
 
 class MyCalender extends StatefulWidget {
-  MyCalender({Key? key, required this.issuccess}) : super(key: key);
-
-  List issuccess;
+  MyCalender({Key? key}) : super(key: key);
 
   @override
   _MyCalender createState() => _MyCalender();
 }
 
 class _MyCalender extends State<MyCalender> {
+  // List<dynamic> issuccess = [];
+
+   getData() async {
+
+    final url = Uri.http("43.200.102.54:8080","/api/user/calendar", {"yearMonth":"2022-07-23"});
+    final req = http.Request("GET", url);
+    req.headers[HttpHeaders.authorizationHeader] = 'Bearer $accessToken';
+
+    final res = await req.send();
+
+    print("res:");
+    print(res.statusCode);
+    // List<dynamic> response = jsonDecode(await res.stream.bytesToString());
+    List<dynamic> issuccess = jsonDecode(await res.stream.bytesToString());
+    print(issuccess);
+    // print(response);
+
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
 
     DateTime now = DateTime.now();
+    getData();
+
 
     return TableCalendar(
       focusedDay: DateTime.now(),
@@ -29,12 +54,13 @@ class _MyCalender extends State<MyCalender> {
         rightChevronVisible: false
       ),
       eventLoader: (day) {
-        for (int i=0;i<widget.issuccess.length; i++) {
+
+        for (int i=0;i<9; i++) {
           if(day.day.toInt() == i+1) {
             return['mark'];
           }
           return[];
-        };
+        }
         return [];
       }
     );
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sikcal/data/shared_preferences.dart';
 import 'package:sikcal/data/constants.dart';
+import 'package:sikcal/screen/mypage/mypage_main_view.dart';
 import 'package:sikcal/screens/auth/auth_gate.dart';
 import 'package:sikcal/screens/main_view.dart';
 
@@ -42,7 +43,7 @@ class _MyAppState extends State<MyApp> {
             textTheme: TextTheme(
               bodyText2: kDefaultTextStyle,
             )),
-        home: Main(),
+        home: MainView(),
       ),
     );
   }
